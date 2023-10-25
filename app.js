946bdf25404b046a65d925aac29c70cb422c5d92
sheep1995 <49236143+sheep1995@users.noreply.github.com>
2023-10-25 22:33:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const jwe = require('./jwe')
 const app = express()
-const port = 8000
+const port = 3000
 const connection = require('./database');
 
 app.use(bodyParser.json());
