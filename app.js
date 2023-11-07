ce5fa8ef8758b2f54caf6cdb15d58e40879e85e9
sheep1995 <49236143+sheep1995@users.noreply.github.com>
2023-11-07 20:13:14 +0800

    update

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwe = require('./jwe')
 const app = express()
 const port = 3000
-//const connection = require('./database');
+const connection = require('./database');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
